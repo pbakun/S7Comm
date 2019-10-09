d9fc50bf8e1dc6618a1e178dc29fee1dd351b3ea
Piotr Bakun <piotr.bakun@gmail.com>
2019-10-09 13:48:05 +0200

    initial commit

---
+++ S7Comm/S7Comm.cs
@@ -0,0 +1,107 @@
+﻿using Sharp7;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S7Comm
+{
+    public class S7Comm
+    {
+        private readonly S7Client _plcConnection;
+        public string ipAddress;
+        private readonly System.Timers.Timer _readTimer;
+        private System.Timers.Timer _toggleTimer;
+        private readonly object _lockObject = new object();
+
+        private byte[] _buffer;
+
+        public S7Comm()
+        {
+            ipAddress = "172.16.0.5";
+            _buffer = new byte[4];
+            _plcConnection = new S7Client();
+
+            var result = _plcConnection.ConnectTo(ipAddress, 0, 1);
+
+            _readTimer = new System.Timers.Timer(1000);
+            _readTimer.Start();
+            _readTimer.Elapsed += _readTimer_Elapsed;
+        }
+
+        private void _readTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            ReadValue();
+        }
+
+        public int ReadValue()
+        { 
+            lock (_lockObject)
+            {
+                _plcConnection.DBRead(10, 0, 4, _buffer);
+
+                int value = S7.GetIntAt(_buffer, 2);
+
+                return value;
+            }
+            
+        }
+
+        public void AddValue()
+        {
+            lock (_lockObject)
+            {
+                S7.SetBitAt(ref _buffer, 0, 0, true);
+
+                _plcConnection.DBWrite(10, 0, 4, _buffer);
+                StartToggleTimer();
+            }
+        }
+
+
+        public void SubValue()
+        {
+            lock (_lockObject)
+            {
+                S7.SetBitAt(ref _buffer, 0, 1, true);
+
+                _plcConnection.DBWrite(10, 0, 4, _buffer);
+                StartToggleTimer();
+            }
+        }
+
+        public void ResetValue()
+        {
+            lock (_lockObject)
+            {
+                S7.SetBitAt(ref _buffer, 0, 2, true);
+
+                _plcConnection.DBWrite(10, 0, 4, _buffer);
+                StartToggleTimer();
+            }
+        }
+
+        private void StartToggleTimer()
+        {
+            _toggleTimer = new System.Timers.Timer(50);
+            _toggleTimer.Start();
+            _toggleTimer.Elapsed += _toggleTimer_Elapsed;
+        }
+
+        private void _toggleTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            _toggleTimer.Stop();
+
+            byte data = new byte();
+            data = 0x00;
+
+            lock (_lockObject)
+            {
+                S7.SetByteAt(_buffer, 0, data);
+                _plcConnection.DBWrite(10, 0, 4, _buffer);
+            }
+            _toggleTimer.Dispose();
+        }
+    }
+}
---
+++ S7CommApp/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using S7Comm;
+
+namespace S7CommApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly S7Comm.S7Comm _plc;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _plc = new S7Comm.S7Comm();
+
+            ValueDisplay.Text = _plc.ReadValue().ToString();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            _plc.AddValue();
+            ValueDisplay.Text = _plc.ReadValue().ToString();
+        }
+
+        private void SubButton_Click(object sender, RoutedEventArgs e)
+        {
+            _plc.SubValue();
+            ValueDisplay.Text = _plc.ReadValue().ToString();
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            _plc.ResetValue();
+            ValueDisplay.Text = _plc.ReadValue().ToString();
+        }
+    }
+}
